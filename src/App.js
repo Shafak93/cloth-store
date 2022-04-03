edc51017a4a26bd78a24abd5c1be183f47b1aa94
Shafak93 <shafakctg64@gmail.com>
2022-04-04 01:36:16 +0600

    navbar style and work

--- src/App.js
+++ src/App.js
@@ -1,18 +1,24 @@
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import About from './Components/About/About';
+import Blogs from './Components/Blogs/Blogs';
 import Dashboard from './Components/Dashboard/Dashboard';
 import Home from './Components/Home/Home';
 import Navbar from './Components/Navbar/Navbar';
+import Reviews from './Components/Reviews/Reviews';
+import bootstrap from 'bootstrap';
+import { Container } from 'react-bootstrap';
 
 function App() {
   return (
-    <div className="App">
+    <div className="App ">
       <Navbar></Navbar>
       <Routes>
          <Route path='/' element={<Home></Home>}></Route>
+         <Route path = '/reviews' element={<Reviews></Reviews>}></Route>
          <Route path='/dashboard' element={<Dashboard></Dashboard>}></Route>
-         <Route></Route>
-         <Route></Route>
+         <Route path='/blogs' element={<Blogs></Blogs>}></Route>
+         <Route path='/about' element={<About></About>}></Route>
       </Routes>
       
     </div>
--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import './Navbar.css'
+import bootstrap from 'bootstrap';
+import { Container } from 'react-bootstrap';
+
 
 const Navbar = () => {
     return (
-        <div>
-            <nav>
-                <Link to='/'>Home</Link>
-                <Link to='dashboard'>Dashboard</Link>
+        <div >
+            <nav className='nav-items'>
+                <Link className='item' to='/'>Home</Link>
+                <Link className='item' to='reviews'>Reviews</Link>
+                <Link className='item' to='dashboard'>Dashboard</Link>
+                <Link className='item' to='blogs'>Blogs</Link>
+                <Link className='item' to='about'>About</Link>
             </nav>
         </div>
     );
