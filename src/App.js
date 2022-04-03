e9c80ab9502e75039a531e8344326c30ad287427
Shafak93 <shafakctg64@gmail.com>
2022-04-04 01:06:31 +0600

    installing rout and bootstrap and creat all component file and folder

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import Dashboard from './Components/Dashboard/Dashboard';
+import Home from './Components/Home/Home';
+import Navbar from './Components/Navbar/Navbar';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar></Navbar>
+      <Routes>
+         <Route path='/' element={<Home></Home>}></Route>
+         <Route path='/dashboard' element={<Dashboard></Dashboard>}></Route>
+         <Route></Route>
+         <Route></Route>
+      </Routes>
+      
     </div>
   );
 }
--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Navbar = () => {
     return (
         <div>
-            
+            <nav>
+                <Link to='/'>Home</Link>
+                <Link to='dashboard'>Dashboard</Link>
+            </nav>
         </div>
     );
 };
