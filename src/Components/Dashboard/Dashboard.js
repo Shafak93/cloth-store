342b063a8a7f0f112178fa76ef6ca5b813c21d9c
Shafak93 <shafakctg64@gmail.com>
2022-04-04 15:22:15 +0600

    creating line and bar chart and chart component

--- src/Components/Dashboard/Dashboard.js
+++ src/Components/Dashboard/Dashboard.js
@@ -1,9 +1,19 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { LineChart, Line, CartesianGrid, XAxis, YAxis, Tooltip } from 'recharts';
+import Chart from '../Chart/Chart';
 
 const Dashboard = () => {
+
+    // const [chartData, setChartData] = useState([]);
+    // useEffect(()=>{
+    //     const url = `https://github.com/ProgrammingHero1/product-analysis-website/blob/main/data.json`;
+    //     fetch(url)
+    //     .then(res => res.json())
+    //     .then(data => setChartData(data))
+    // },[])
     return (
-        <div>
-            <h1>THis is dashboard</h1>
+        <div className='container'>
+            <Chart></Chart>
         </div>
     );
 };
